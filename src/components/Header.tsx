6a112722e6219069b1cde815ab4a297113c80212
igovor01 <igovor01@fesb.hr>
2023-10-24 18:56:28 +0200

    navbar dropdown done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,19 @@ class Header extends React.Component<{}, {}> {
     return (
       <nav className="header">
         <ul>
-          <li>
-            <a href="/">Početna</a>
-          </li>
+          <li><a href="/">Početna</a></li>
           <li>
             <a href="/">Nacionalni parkovi</a>
-            <div className="dropdown">
-              <a href="">Link 1</a>
-              <a href="">Link 2</a>
-              <a href="">Link 3</a>
-            </div>
+            <ul className="dropdown">
+              <li><a href="">NP Brijuni</a></li>
+              <li><a href="">NP Krka</a></li>
+              <li><a href="">NP Paklenica</a></li>
+              <li><a href="">NP Plitvička jezera</a></li> 
+              <li><a href="">NP Kornati</a></li>   
+              <li><a href="">NP Mljet</a></li>  
+              <li><a href="">NP Risnjak</a></li>  
+              <li><a href="">NP Sjeverni Velebit</a></li>     
+            </ul>
           </li>
           <li>
             <a href="/">Komentari</a>
